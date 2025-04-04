d030bfa7c8b1d1af0f94fd7855f700916e354034
ISinFor7 <159419857+ISinFor7@users.noreply.github.com>
2025-04-04 13:33:46 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,6 @@ opam install ocaml-lsp-server
 opam install ocamlformat
 tar xfz proj_graphdb.tgz
 cd Proj_GraphDB/
-opam init
-eval 
 opam install dune
 opam install ppx_deriving
 opam install ocamlgraph
